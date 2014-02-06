512a301a888e43400c9686b0948af5c83acca427
新居 佳英 <IGP01@shinkyo-yoei-no-MacBook-Pro.local>
2014-02-06 20:06:33 +0900

    iroiro

--- js/common.js
+++ js/common.js
@@ -3,11 +3,26 @@ function AlbumCtrl($scope,$timeout) {
     $scope.onTimeout = function(){
       $scope.counter--;
       mytimeout = $timeout($scope.onTimeout,1000);
-      if($scope.counter == 170){alert("test");}
+      if($scope.counter == 150){console.log("first change");}
+      if($scope.counter == 120){console.log("second change");}
+      if($scope.counter == 90){console.log("third change");}
+      if($scope.counter == 60){console.log("turn yellow");}
+      if($scope.counter == 30){console.log("turn red");}
+      if($scope.counter == 0){console.log("go out ultraman");}
+      if($scope.counter == -3){console.log("gozilla get ramen");}
     }
     var mytimeout = $timeout($scope.onTimeout,1000);
-    $scope.stop = function(){
-      $timeout.cancel(mytimeout);
-    }
+}
 
+function ImageArea($scope) {
+  $scope.gozillaCounter = 0;
+  $scope.ultramanCounter = 0;
+  $scope.gozillaCountUp = function(){
+    $scope.gozillaCounter++
+    console.log($scope.gozillaCounter);
+  }
+  $scope.ultramanCountUp = function(){
+    $scope.gozillaCounter++
+    console.log($scope.gozillaCounter);
+  }
 }
